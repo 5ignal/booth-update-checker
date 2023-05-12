efaae6ef0c7c21b2f57fc3e196d304bf3a77c236
5ignal <ogunaru@wakamo.moe>
2023-05-12 20:01:54 +0900

    app.py 수정

--- app-sample.py
+++ app-sample.py
@@ -1,6 +1,7 @@
 import os.path
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
 
 
 def parser(order_num, cookie):
@@ -9,25 +10,23 @@ def parser(order_num, cookie):
     html = response.content
     soup = BeautifulSoup(html, "html.parser")
     divs = soup.find_all("div", class_="u-ml-500")
-    download_url = list()
+    download_url_list = list()
     for div in divs:
         download_link = div.select_one("a.nav-reverse")
         href = download_link.get("href")
-        download_url.append(href)
-    if download_link == []:
-        print("BOOTH no response")
-    return download_url
+        download_url_list.append(href)
+    return download_url_list
 
 
-def file_save(version_file, order_num):
-    f = open(f'./version/{order_num}.txt', "w")
-    for result in version_file:
+def file_save(version_file_path, download_url_list, order_num):
+    f = open(version_file_path, "w")
+    for result in download_url_list:
         f.write(result + "\n")
     f.close()
     print(f'{order_num} version file saved')
 
 
-def discord_webhook(webhook_url, name, content):
+def discord_webhook(webhook_url, content, name):
     data = {
         "username" : "부스 업데이트 체크",
         "content" : (f'{name} {content}')
@@ -35,22 +34,37 @@ def discord_webhook(webhook_url, name, content):
     requests.post(webhook_url, json=data)
 
 
-def update_cheker(name, url, order_num, cookie, version_file, webhook_url):
-    download_url = parser(order_num, cookie)
-    version_file = (f'./version/{order_num}.txt')
-    if os.path.exists(version_file):
-        with open(version_file) as f:
-            version_file = f.readlines()
-        version_file = [line.rstrip('\n') for line in version_file]
-        if sorted(version_file) != sorted(download_url):
-            result = (f'업데이트 있음 \n {url}')
-            print(f'{name} 업데이트 있음')
-            discord_webhook(webhook_url, name, result)
-        else:
-            print(f'{name} 업데이트 없음')
-        file_save(version_file, order_num)
+def discord_webhook(webhook_url, content):
+    data = {
+        "username" : "부스 업데이트 체크",
+        "content" : content
+    }
+    requests.post(webhook_url, json=data)
+
+
+def update_cheker(name, url, order_num, cookie, webhook_url):
+    download_url_list = parser(order_num, cookie)
+    if download_url_list == []:
+        result = "BOOTH no response"
+        discord_webhook(webhook_url, result)
     else:
-        file_save(version_file, order_num)
+        version_file_path = (f'./version/{order_num}.txt')
+        if os.path.exists(version_file_path):
+            with open(version_file_path) as f:
+                version_list = f.readlines()
+            version_list = [line.rstrip('\n') for line in version_list]
+            print(f'LOCAL : {version_list}')
+            print(f'BOOTH : {download_url_list}')
+            if sorted(version_list) != sorted(download_url_list):
+                result = (f'업데이트 있음 \n {url}')
+                print(f'{name} 업데이트 있음')
+                discord_webhook(webhook_url, result, name)
+            else:
+                print(f'{name} 업데이트 없음')
+            file_save(version_file_path, download_url_list, order_num)
+        else:
+            print("version file not found")
+            file_save(version_file_path, download_url_list, order_num)
 
 
 if __name__ == "__main__":
@@ -58,16 +72,23 @@ if __name__ == "__main__":
     booth_cookie = {"_plaza_session_nktz7u": "(BOOTH 계정 쿠키 값)"}
     discord_webhook_url = "(디스코드 Webhook URL)"
 
-    # 아바타_0
-    booth_name = "아바타_0"
-    booth_url = "BOOTH 상품 페이지 URL"
-    booth_order_number = "BOOTH 주문 번호"
-    version_server_url = "app.py에서 생성된 파일 URL"
-    update_cheker(booth_name, booth_url, booth_order_number, booth_cookie, version_server_url, discord_webhook_url)
+    # 갱신 간격 (초)
+    refresh_interval = 600
 
-    # 아바타_1
-    booth_name = "아바타_1"
-    booth_url = "BOOTH 상품 페이지 URL"
-    booth_order_number = "BOOTH 주문 번호"
-    version_server_url = "app.py에서 생성된 파일 URL"
-    update_cheker(booth_name, booth_url, booth_order_number, booth_cookie, version_server_url, discord_webhook_url)
+    while True:
+        # 아바타_0
+        booth_name = "아바타_0"
+        booth_url = "BOOTH 상품 페이지 URL"
+        booth_order_number = "BOOTH 주문 번호"
+        update_cheker(booth_name, booth_url, booth_order_number, booth_cookie, discord_webhook_url)
+
+        # 아바타_1
+        booth_name = "아바타_1"
+        booth_url = "BOOTH 상품 페이지 URL"
+        booth_order_number = "BOOTH 주문 번호"
+        update_cheker(booth_name, booth_url, booth_order_number, booth_cookie, discord_webhook_url)
+
+        # 갱신 대기
+        print("waiting for refresh")
+        sleep(refresh_interval)
+        
