792d8981cbee75b5d78239b75c8e90b262905b2e
Ogunaru <72740815+5ignal@users.noreply.github.com>
2024-10-22 10:35:16 +0900

    Refactor: Update booth_sqlite.py to use booth_accounts table for booth_items

--- booth_checker/booth_sqlite.py
+++ booth_checker/booth_sqlite.py
@@ -6,15 +6,17 @@ class BoothSQLite():
         self.cursor = self.conn.cursor()
 
         self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS session_cookies (
-                session_cookie TEXT,
-                discord_user_id TEXT PRIMARY KEY
+            CREATE TABLE IF NOT EXISTS booth_accounts (
+                session_cookie TEXT UNIQUE,
+                discord_user_id TEXT PRIMARY KEY,
+                discord_channel_id TEXT
             )
         ''')
 
         self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS items (
+            CREATE TABLE IF NOT EXISTS booth_items (
                 session_cookie TEXT,
+                discord_channel_id TEXT,
                 booth_order_number TEXT PRIMARY KEY,
                 booth_item_name TEXT,
                 booth_check_only TEXT,
@@ -22,49 +24,43 @@ class BoothSQLite():
                 download_number_show BOOLEAN,
                 changelog_show BOOLEAN,
                 archive_this BOOLEAN,
-                FOREIGN KEY(session_cookie) REFERENCES session_cookies(session_cookie)
+                FOREIGN KEY(session_cookie, discord_channel_id) REFERENCES booth_accounts(session_cookie, discord_channel_id)
             )
         ''')
 
     def __del__(self):
         self.conn.close()
 
-    def get_items(self):
+    def get_booth_items(self):
         self.cursor.execute('''
-            SELECT * FROM items
+            SELECT * FROM booth_items
         ''')
         return self.cursor.fetchall()
-    
-    def get_check_only(self, booth_order_number):
-        self.cursor.execute('''
-            SELECT * FROM booth_check_only WHERE item_id = ?
-        ''', (booth_order_number,))
-        return self.cursor.fetchall()
 
-    def get_session_cookie_by_discord_user_id(self, discord_user_id):
+    def get_booth_accounts(self, discord_user_id):
         self.cursor.execute('''
-            SELECT session_cookie FROM session_cookies
+            SELECT session_cookie, discord_channel_id FROM booth_accounts
             WHERE discord_user_id = ?
         ''', (discord_user_id,))
         result = self.cursor.fetchone()
         if result:
-            return result[0]
+            return result
         return None
     
-    def add_session_cookie(self, session_cookie, discord_user_id):
+    def add_booth_accounts(self, session_cookie, discord_user_id, discord_channel_id):
         self.cursor.execute('''
-            INSERT INTO session_cookies (session_cookie, discord_user_id)
-            VALUES (?, ?)
-        ''', (session_cookie, discord_user_id))
+            INSERT OR REPLACE INTO session_cookies (session_cookie, discord_user_id, discord_channel_id)
+            VALUES (?, ?, ?)
+        ''', (session_cookie, discord_user_id, discord_channel_id))
         self.conn.commit()
         return self.cursor.lastrowid
     
     def add_item(self, discord_user_id, booth_order_number, booth_item_name, intent_encoding, download_number_show, changelog_show, archive_this):
-        session_cookie = self.get_session_cookie_by_discord_user_id(discord_user_id)
-        if session_cookie:
+        booth_account = self.get_booth_accounts(discord_user_id)
+        if booth_account:
             self.cursor.execute('''
-                INSERT INTO items (session_cookie, booth_order_number, booth_item_name, intent_encoding, download_number_show, changelog_show, archive_this)
-                VALUES (?, ?, ?, ?, ?, ?, ?)
-            ''', (session_cookie, booth_order_number, booth_item_name, intent_encoding, download_number_show, changelog_show, archive_this))
+                INSERT INTO items (session_cookie, discord_channel_id, booth_order_number, booth_item_name, intent_encoding, download_number_show, changelog_show, archive_this)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (booth_account[0], booth_account[1], booth_order_number, booth_item_name, intent_encoding, download_number_show, changelog_show, archive_this))
             self.conn.commit()
             return self.cursor.lastrowid
